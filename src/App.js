4ee090e70b48fc58524462fa26282681a69c37f8
Lara <larabernspereira@gmail.com>
2023-08-27 23:07:37 -0300

    add arquivos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [endereco, setEndereco] = useState({})
+
+  function manipularEndereco(evento) {
+
+    const cep = evento.target.value
+
+    setEndereco({
+      cep
+    })
+  }
+
+  function atualizaDados(evento) {
+    evento.preventDefault()
+
+    if (endereco.cep.length === 8) {
+      fetch(`https://viacep.com.br/ws/${endereco.cep}/json/`)
+        .then(resposta => resposta.json())
+        .then(dados => {
+          setEndereco({
+            cep: dados.cep,
+            rua: dados.logradouro,
+            bairro: dados.bairro,
+            cidade: dados.localidade,
+            estado: dados.uf,
+            ddd: dados.ddd
+          })
+        })
+    } else {
+      alert("CEP inválido. Tente novamente! Digite sem caracteres especiais, por favor")
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Busca CEP</h2>
+      <input placeholder='Digite o CEP - 8 dígitos' onChange={manipularEndereco}></input>
+      <ul className='Lista'>
+        <li>
+          CEP: <span>{endereco.cep} </span>
+        </li>
+        <li>
+          Bairro: <span>{endereco.bairro}</span>
+        </li>
+        <li>
+          Cidade: <span>{endereco.cidade}</span>
+        </li>
+        <li>
+          Estado: <span>{endereco.estado}</span>
+        </li>
+        <li>
+          DDD:<span>{endereco.ddd} </span>
+        </li>
+      </ul>
+      <button
+        onClick={(evento) => atualizaDados(evento)}
+        className='botao'
+      >
+        Buscar
+      </button>
     </div>
   );
 }
